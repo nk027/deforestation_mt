533d301ef493e486935cb7386685082997a86d4a
Nikolas Kuschnig <nikolas.kuschnig@wu.ac.at>
2019-04-10 13:16:29 +0000

    Add some information on vars

--- R/2_wrap_up.R
+++ R/2_wrap_up.R
@@ -8,5 +8,18 @@ library(sf)
 shp <- readRDS("data/geo/shp.rds")
 tab <- readRDS("data/tab/tab.rds")
 
-data <- full_join(shp, tab, by = c("code", "date"))
+data <- full_join(shp, tab, by = c("code", "date")) %>% 
+  filter(date < 2018)
+
+refcols <- c("code", "name", "date")
+data <- data[, c(refcols, setdiff(names(data), refcols))]
+
 saveRDS(data, "data/data.rds")
+
+
+# Generate some summaries -------------------------------------------------
+
+vars <- read.table("txt/variables.txt", stringsAsFactors = FALSE)
+
+summaries <- t(apply(vars, 1, function(y) c(summary(data[[y]]))))
+write.table(summaries, "txt/summaries.txt", quote = FALSE, row.names = FALSE)
