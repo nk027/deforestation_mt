63d5c3c462bfd547ceb54237fc99a8f5dbeea814
Nikolas Kuschnig <nikolas.kuschnig@wu.ac.at>
2019-04-24 14:53:04 +0000

    Finish iiasa scripts with base vars

--- R/1_merge_iiasa.R
+++ R/1_merge_iiasa.R
@@ -14,11 +14,11 @@ shp <- subset(shp, shp$CD_GEOCMU < 5200000, drop = TRUE)
 
 extr <- extract(r, shp, df = TRUE, weights = TRUE)
 
-data <- extr %>% group_by(ID, raster) %>% 
+extr_df <- extr %>% group_by(ID, raster) %>% 
   summarise(sum(weight))
-names(data) <- c("id", "simu", "weight")
+names(extr_df) <- c("id", "simu", "weight")
 
-data$code <- shp$CD_GEOCMU[extr$id]
+extr_df$code <- shp$CD_GEOCMU[extr$id]
 
 
 # CSV ---------------------------------------------------------------------
@@ -34,8 +34,30 @@ grep("^SugC..", names(yields))
 grep("^Sunf..", names(yields))
 grep("^Soya..", names(yields))
 
-data <- left_join(data, yields, by = c("simu" = "SIMUID"))
 
 time <- read_csv("data/other/acc_travel_min.csv")
 
-data <- left_join(data, time, by = c("simu" = "VALUE"))
+data <- extr_df %>% 
+  left_join(yields, by = c("simu" = "SIMUID")) %>% 
+  left_join(time, by = c("simu" = "VALUE"))
+
+# Some NAs for yields, fewer for travel time
+data[is.na(data$MEANYLD), ]$weight
+data[is.na(data$MEAN1), ]$weight
+# Remove NAs
+data <- data[!is.na(data$MEANYLD) & !is.na(data$MEAN1), ]
+# Recalculate weights
+reweigh <- data %>% group_by(code) %>% 
+  summarise(reweigh = sum(weight))
+data_wt <- left_join(data, reweigh, by = "code") %>% 
+  mutate(reweight = weight / reweigh)
+# Check
+data_wt %>% group_by(code) %>% 
+  summarise(check = round(sum(reweight), 1)) %>% .$check == 1
+
+# Aggregate and finalise
+data_wt %>% 
+  transmute(code, yield = reweight * MEANYLD, travel = reweight * MEAN1) %>% 
+  group_by(code) %>% 
+  summarise(yield = sum(yield), travel = sum(travel)) %>% 
+  saveRDS("data/other/iiasa.rds")
