754645f34567177a078399bd847781adff885d4d
Nikolas K <nikolas.kuschnig@wu.ac.at>
2020-03-14 12:49:16 +0100

    Create 81_morans.R

---
+++ R/81_morans.R
@@ -0,0 +1,23 @@
+
+# Dependencies ------------------------------------------------------------
+
+stopifnot(
+  # Needs 31
+  exists("data"), exists("dates"), exists("dates_len"), exists("W_qu"),
+  require("spdep")
+)
+
+
+# Get Moran's statistics --------------------------------------------------
+
+dep <- data$forest_ch_km2[data$date >= dates[1] & 
+                          data$date <= dates[dates_len]]
+listw <- mat2listw(kronecker(diag(dates_len), W_qu))
+
+(mor <- moran.test(dep, listw))
+
+(mor_mc <- moran.mc(dep, listw, 10000L))
+
+(mor_loc <- localmoran(dep, listw))
+
+summary(mor_loc)
